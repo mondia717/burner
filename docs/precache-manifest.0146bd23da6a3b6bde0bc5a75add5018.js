a68407092afcb9da5bb593b9508b47acb261cac5
mondia717 <mondia717@protonmail.com>
2020-05-08 18:28:01 +0200

    commit5

---
+++ docs/precache-manifest.0146bd23da6a3b6bde0bc5a75add5018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "147f5463ff565d70c532",
+    "url": "/static/css/main.4fbe0d6b.chunk.css"
+  },
+  {
+    "revision": "147f5463ff565d70c532",
+    "url": "/static/js/main.d76d95dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "00e0c94288cc2705d1b5",
+    "url": "/static/js/2.981292fe.chunk.js"
+  },
+  {
+    "revision": "72ddd9e3a209a2f87f135805b75b8b1d",
+    "url": "/static/media/TronLinkLogo.72ddd9e3.png"
+  },
+  {
+    "revision": "62c98da9ea7f84b3351effd1cdbc7c75",
+    "url": "/index.html"
+  }
+];
