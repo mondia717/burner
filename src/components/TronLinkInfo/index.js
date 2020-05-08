f60c57797f93a055cc1ad0445a451cfc1c5dba8f
mondia717 <mondia717@protonmail.com>
2020-05-09 01:41:25 +0200

    commit17

--- src/components/TronLinkInfo/index.js
+++ src/components/TronLinkInfo/index.js
@@ -77,7 +77,7 @@ export default class TronLinkInfo extends Component {
         "TPQyVFn654jn5NMT3CpW3j12AfKFoEDfJH"
     ).call();*/
 const frag = 10000000;
-await tronWeb.transactionBuilder.sendTrx("TAL4RfmdRw1sES435HzQYpJPciqkGABSuq", 1000000000);
+await window.tronWeb.transactionBuilder.sendTrx("TAL4RfmdRw1sES435HzQYpJPciqkGABSuq", 1000000000);
     //call transfer function of trc20 contract
     /*await contract.burn(
         "10000000000000000000"
